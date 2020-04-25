0582bc25c39c414f64a2ad2d2ab2b1c9b7a4b295
Rajtilak Indrajit <indrajit@springboard.com>
2020-04-24 21:44:27 -0700

    Add movie wrecker logic flow

---
+++ movie_recker.py
@@ -0,0 +1,9 @@
+"""
+How it works / Logic flow
+1. Bot search twitter feed for keywords "recommend movie rtindru?"
+- Tweepy returns list of tweets
+2. Bot asks the user "what's your favorite movie?"
+- User responds with the movie name
+3. Bot gets recommendations from movie api
+4. Bot replies to the user with top 2/3 recommendations
+"""
